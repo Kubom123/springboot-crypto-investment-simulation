24110302b2301211dd516f525154d8ad78e4a7bb
Kubom123 <jackobmiklush@gmail.com>
2022-09-11 18:27:52 +0200

    iteration no.1

---
+++ src/test/java/com/springboot/springbootcryptoinvestmentsimulation/SpringbootCryptoInvestmentSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootcryptoinvestmentsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCryptoInvestmentSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
